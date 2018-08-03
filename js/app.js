7f317a4fc97e17223b952e558fd2e76e5f04b643
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-08-02 23:54:32 -1000

    show imgs or videos or none dep on type of url

--- js/app.js
+++ js/app.js
@@ -23,13 +23,26 @@ request(url, res => {
     document.getElementById("bodyElem").appendChild(listE);
 
     //get images
-    let img = document.createElement("img");
-    img.src = dataL[i].data.url;
-    img.className = "imgL";
-    listE.appendChild(img);
+
+    let linkType = dataL[i].data.post_hint;
+
     // console.log(img);
-    let test = dataL[i].data.stickied;
-    console.log("test", test);
+
+    console.log("test", linkType);
+
+    if (linkType === "image") {
+      let img = document.createElement("img");
+      img.src = dataL[i].data.url;
+      img.className = "imgL";
+      listE.appendChild(img);
+    } else if (linkType === "rich:video") {
+      let vid = document.createElement("video");
+      vid.autoplay = true;
+      vid.controls = true;
+      vid.src = dataL[i].data.url;
+      listE.appendChild(vid);
+      console.log("fuck");
+    }
     //get comments
     // console.log(dataL[i].data.url);
     let commentL = document.createElement("li");
